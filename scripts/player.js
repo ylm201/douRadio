6f548da569bcf9fddc38fec7503cabfdcf6e559e
Alex.Yang <lmyang.alex@gmail.com>
2011-11-22 21:43:41 +0800

    初步完成OAuth

--- scripts/player.js
+++ scripts/player.js
@@ -10,11 +10,39 @@
 			}
 			$("#play").html(radio.power()==true?"off":"on")
 	};
-	function make_base_auth(user, password) {
-			var tok = user + ':' + password;
-			hash=$.base64.encode(tok);
-			 return "Basic " + hash;
-	}; 
+	
+	//构造签名头部
+	function getRequestHeader(data){
+		var message = {
+		    method: "POST",
+		    action: "http://api.fanfou.com/statuses/update.json",	
+		    parameters: {
+		        oauth_consumer_key: "327fe47f56d57ead9539c3498772fc3d",
+		        oauth_token: localStorage["fanfou_access_token"],
+				oauth_signature_method: "HMAC-SHA1",
+		        oauth_signature: "",
+		        oauth_timestamp: "",
+		        oauth_nonce: "",
+				status:data
+			  }
+		  }
+		  // 签名
+		  OAuth.setTimestampAndNonce(message);
+		  OAuth.SignatureMethod.sign(message, {
+		      consumerSecret: "688edd10e5a2c923cf9421a644240e70",
+		      tokenSecret: localStorage["fanfou_access_token_secret"],
+		  });
+
+		//构造OAuth头部
+		var oauth_header = "OAuth realm=\"\", oauth_consumer_key=";
+		oauth_header += message.parameters.oauth_consumer_key + ', oauth_nonce=';
+		oauth_header += message.parameters.oauth_nonce + ', oauth_timestamp=';
+		oauth_header += message.parameters.oauth_timestamp + ', oauth_signature_method=HMAC-SHA1, oauth_signature=';
+		oauth_header += message.parameters.oauth_signature + ', oauth_token=';
+		oauth_header += message.parameters.oauth_token;	
+		return oauth_header		
+	}
+	
 	$("#skip").bind("click",function(){
 			radio.skip();
 			showSong();
@@ -48,14 +76,16 @@
 	});
 	$('#submit').bind("click",function(){
 		var value=$("#comment")[0].value;
+		var content=' #豆瓣电台# '+radio.getSong().artist+'--'+radio.getSong().title+' '+value
+
 		$.ajax({
 				url : "http://api.fanfou.com/statuses/update.json",
 				type : "POST",
 				data:{
-					status:' #豆瓣电台# '+radio.getSong().artist+'--'+radio.getSong().title+' '+value
+					status:content
 				},
 				beforeSend : function(req) {
-				req.setRequestHeader('Authorization', make_base_auth('ylm201@126.com','58814023'));
+				req.setRequestHeader('Authorization',getRequestHeader(content));
 				},
 				error:function(XmlHttpRequest,textStatus,errortown){
 					alert(XMLHttpRequest);
