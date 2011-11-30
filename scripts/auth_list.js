92eea021758e5f5982fa4b4a1333fcef3eda95f3
Alex.Yang <lmyang.alex@gmail.com>
2011-11-30 21:16:26 +0800

    OAuth1.0通用模块

---
+++ scripts/auth_list.js
@@ -0,0 +1,17 @@
+﻿var fanfou={
+	request_token_url: "http://fanfou.com/oauth/request_token",
+	access_token_url:  "http://fanfou.com/oauth/access_token",
+	authorization_url: "http://fanfou.com/oauth/authorize?oauth_token=",
+	api_key:"327fe47f56d57ead9539c3498772fc3d",
+	api_key_secret:"688edd10e5a2c923cf9421a644240e70",
+	prefix:"fanfou" 
+}
+
+var sina={
+	request_token_url: "http://fanfou.com/oauth/request_token",
+	access_token_url:  "http://fanfou.com/oauth/access_token",
+	authorization_url: "http://fanfou.com/oauth/authorize?oauth_token=",
+	api_key:"694135578",
+	api_key_secret:"683f9dd0a5d78c5488b7460a42e654c3",
+	prefix:"sina" 
+}
--- scripts/oauth_helper.js
+++ scripts/oauth_helper.js
@@ -15,6 +15,12 @@ var OAuthHelper=function(arg){
 	this.prefix=arg.prefix	
 }
 
+
+OAuthHelper.prototype.auth=function(){
+	this.getRequestToken()
+
+}
+
 OAuthHelper.prototype.getRequestToken=function(){
 	var message = {
 		method: "GET",
@@ -32,16 +38,18 @@ OAuthHelper.prototype.getRequestToken=function(){
 	OAuth.SignatureMethod.sign(message, {
 		consumerSecret: this.api_key_secret
 	})
-
+	var self=this
 	$.get(message.action,message.parameters,function(response){
 		var responseObj = OAuth.getParameterMap(OAuth.decodeForm(response));				
-		this.request_token = responseObj.oauth_token
-		this.request_token_secret = responseObj.oauth_token_secret
-		console.log("request token: "this.request_token_secret)
-		console.loga("request token secret: "this.request_token_secret)
-		var url=authorization_uri + request_token+"&oauth_callback="+encodeURI(window.location.href);		
-		chrome.tabs.create({url:url})
-		this.onAuthorized()	
+		self.request_token = responseObj.oauth_token
+		self.request_token_secret = responseObj.oauth_token_secret
+		console.log("request token: "+self.request_token)
+		console.log("request token secret: "+self.request_token_secret)
+		var url=self.authorization_uri + self.request_token+"&oauth_callback="+encodeURI(window.location.origin+"/oauth_callback.htm");	
+		console.log(url)
+		chrome.tabs.create({url:url},function(tab){
+			self.onAuthorized()	
+		})
 	})									
 }
 
@@ -50,20 +58,24 @@ OAuthHelper.prototype.getRequestToken=function(){
  * */
 OAuthHelper.prototype.onAuthorized=function(){
 	var self=this
-	chrome.tabs.onUpdated.addListener(function(tabId,info,tab){
-		if(info.url.endWith("oauth_callback"){
+	//var call_back_page=window.location.origin+"/oauth_callback.htm"
+	chrome.tabs.onUpdated.addListener(function(tabId,info,tab){	
+		var b=tab.url.indexOf(self.authorization_uri)
+		console.log("vaildate result:"+b)
+		if(info.status=="loading"&&tab.url.indexOf("oauth_callback.htm")>-1&&tab.url.indexOf(self.authorization_uri)<0){
+			console.log("updated....."+tab.url)
 			self.getAccessToken()
 		}
 	})
 }
 
-OAuthHelper.getAccessToken=function(){
+OAuthHelper.prototype.getAccessToken=function(){
 	var message = {
 		method: "GET",
-		action: this,access_token_uri,
+		action: this.access_token_uri,
 		parameters: {
 			oauth_consumer_key: this.api_key,
-			oauth_token:this.request_token 
+			oauth_token:this.request_token, 
 			oauth_signature_method: this.signature_method,
 			oauth_signature: "",
 			oauth_timestamp: "",
@@ -71,16 +83,21 @@ OAuthHelper.getAccessToken=function(){
 		}
 	}
 	OAuth.setTimestampAndNonce(message);
+	console.log(this.api_key_secret)
+	console.log(this.request_token_secret)
 	OAuth.SignatureMethod.sign(message, {
 		consumerSecret: this.api_key_secret,
-		tokenSecret: this.request_key_secret
+		tokenSecret: this.request_token_secret
 	});
 	var self=this
-	$.get(access_token_uri,message.parameters,function(response){
+	$.get(message.action,message.parameters,function(response){
 		var responseObj = OAuth.getParameterMap(OAuth.decodeForm(response));
 		access_token = responseObj.oauth_token
 		access_token_secret = responseObj.oauth_token_secret
+		console.log("access_token:"+access_token)
+		console.log("access_token_secret:"+access_token_secret)
 		localStorage[self.prefix+"_access_token"]=access_token
-		localStorage[self.prefix+"_access_secret"]=access_token_secret			
+		localStorage[self.prefix+"_access_secret"]=access_token_secret
+		alert("success")
 	})		
 }
