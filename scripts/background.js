f0795bea99beeecc36dd19ad29246478e1fef907
Yang LiMing <lmyang.alex@gmail.com>
2012-11-30 18:53:16 +0800

    up

--- scripts/background.js
+++ scripts/background.js
@@ -32,3 +32,13 @@ chrome.extension.onRequest.addListener(function(req,sender,resp){
 		}
 	}
 });
+
+(function(){
+	var _gaq = _gaq || [];
+	 _gaq.push(['_setAccount', 'UA-27166419-3']);
+	(function() {
+		 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		 ga.src = 'https://ssl.google-analytics.com/ga.js';
+		 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+})();
