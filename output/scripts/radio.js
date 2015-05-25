ba3090ffb2d11462d949a0b21b1b8651df0ccdfb
礼明 杨 <alex@limingdeMBP.lan>
2015-05-26 00:34:38 +0800

    remove analytics code

--- output/scripts/radio.js
+++ output/scripts/radio.js
@@ -2,7 +2,3 @@ seajs.config({
   base: "/static/",
 });
 seajs.use("background/1.0.0/background");
-window.onerror = function(message, url, line, col) {
-	console.error(message);
-	_gaq.push(['_trackEvent', 'JsError', url + ':' + line + ':'+col+':' + message, chrome.app.getDetails().version]);
-}
